78e92e87649208280eed3d744b0fd1a6f367c209
Viorel Ciucu <viorel.ciucu@gmail.com>
2024-05-26 13:42:53 +0200

    Fixing Stefano's bugs.

--- Install-PSModules.ps1
+++ Install-PSModules.ps1
@@ -1,11 +1,20 @@
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-Install-Module -Name dbatools -Scope CurrentUser
-Install-Module -Name Pester -Force -SkipPublisherCheck
-Install-Module -Name Plaster -Scope CurrentUser
-Install-Module -Name HtmlReport -Scope CurrentUser
-Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
-Install-Module -Name posh-git -Scope CurrentUser
-Install-Module -Name oh-my-posh -Scope CurrentUser
-Install-Module -Name PSWindowsUpdate -Scope CurrentUser
-Install-Module -Name ImportExcel -Scope CurrentUser
+$modules = @(
+    'dbatools',
+    'Pester',
+    'Plaster',
+    'HtmlReport',
+    'PSScriptAnalyzer',
+    'posh-git',
+    'oh-my-posh',
+    'PSWindowsUpdate',
+    'ImportExcel',
+    'DockerCompletion'
+)
+
+foreach ($module in $modules) {
+    if (-not (Get-Module -Name $module -ListAvailable)) {
+        Install-Module -Name $module -Scope CurrentUser -Force -SkipPublisherCheck
+    }
+}
